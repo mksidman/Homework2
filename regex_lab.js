2352cc0004159f5402ffda3294910b170b844a2b
mksidman <mksidman@gmail.com>
2016-03-23 19:28:17 -0700

    Update regex_lab.js
    Updated with corrections from class

--- regex_lab.js
+++ regex_lab.js
@@ -1,39 +1,67 @@
 var animalsArray = ["Cat", "Dog", "Polar Bear", "Grizzly Bear", "Antelope", "Kangaroo", "Bear"];
 
-//Array with animals that have three letter names
-var threeLetters = animalsArray.filter(function(animal) {
-  return animal == animal.match(/[A-z]{3}/i);
-});
-// console.log(threeLetters);
+
+//Array with animals that have three letter names executes a conditional in the callback
+var threeLetterPattern = /^[A-z]{3}$/i;
+
+var threeLetterAnimals = animalsArray.filter(function(animal) {
+  return threeLetterPattern.test(animal);
+})
+
+console.log(threeLetterAnimals);
+
 
 //Array with animals that have two-word names
-var twoWords = animalsArray.filter(function(animal) {
-  return animal == animal.match(/[A-z]+ [A-z]+/);
+var twoWordPattern = /[A-z]+ [A-z]+/i;
+
+var twoWordAnimals = animalsArray.filter(function(animal) {
+  return twoWordPattern.test(animal);
 })
-// console.log(twoWords);
+
+console.log(twoWordAnimals);
+
 
 // Array with only animals that don't start with K
-var letterK = animalsArray.filter(function(animal) {
-  return animal != animal.match(/^K/);
-})
-// console.log(letterK);
+var letterKPattern = /^k/i;
+var notStartingWithK = /^[^k]/i; //how to write not starting with k in regex
 
+var noLetterK = animalsArray.filter(function(animal) {
+  return !letterKPattern.test(animal);
+})
+
+console.log(noLetterK);
+
+
+// replace bear with teddy bear
 var teddyBear = animalsArray.map(function(animal) {
-  if (animal.match(/bear/i)) {
-    return animal = "Teddy Bear";
-  } else {
-    return animal;
-  }
+  return animal.replace(/bear/ig, "Teddy Bear");
 })
-// console.log(teddyBear);
 
+
+// var teddyBear = animalsArray.map(function(animal) {
+//   if (animal.match(/bear/i)) {
+//     return animal = "Teddy Bear";
+//   } else {
+//     return animal;
+//   }
+// })
+
+console.log(teddyBear);
+
+
+// replace cat with kitty cat and dog with puppy
 var catDog = animalsArray.map(function(animal) {
-  if (animal.match(/cat/i)) {
-    return animal = "Kitty Cat";
-  } else if (animal.match(/dog/i)) {
-    return animal = "Puppy";
-  } else {
-    return animal;
-  }
+  return animal.replace(/cat/i, "Kitty Cat").replace(/dog/i, "Puppy");
 })
+
+// var catDog = animalsArray.map(function(animal) {
+//   if (animal.match(/cat/i)) {
+//     return animal = "Kitty Cat";
+//   } else if (animal.match(/dog/i)) {
+//     return animal = "Puppy";
+//   } else {
+//     return animal;
+//   }
+// })
+
 console.log(catDog);
